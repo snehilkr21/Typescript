0c4526da9036d47609f4b6ddeb79f04b2f0cfb04
snehil <snehil@snehils-MacBook-Air.local>
2024-09-06 13:12:59 +0530

    commit 3 enum

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-var isBeginner = true;
-console.log(isBeginner);
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 10] = "Up";
+    Direction[Direction["down"] = 11] = "down";
+    Direction[Direction["left"] = 25] = "left";
+    Direction[Direction["right"] = 26] = "right";
+})(Direction || (Direction = {}));
+var move = Direction.Up;
+move = Direction.right;
+console.log(move);
--- main.ts
+++ main.ts
@@ -1,2 +1,9 @@
-let isBeginner : boolean = true;
-console.log(isBeginner)
+enum Direction {
+    Up=10,
+    down,
+    left=25,
+    right
+}
+let move :Direction = Direction.Up
+move = Direction.right
+console.log(move)
