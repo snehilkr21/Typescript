828b463b2a122bef5d3f946b9477a516453e714b
snehil <snehil@snehils-MacBook-Air.local>
2024-09-29 14:26:26 +0530

    function with params and return an object

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function isCheck(_a) {
-    var amount = _a.amount, isPaid = _a.isPaid;
+function isCheck(obj) {
+    console.log("obj ", obj);
     return { amount: 1000, isPaid: false };
 }
 var data = isCheck({ amount: 1000, isPaid: false });
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-function isCheck({ amount, isPaid }: { amount: number; isPaid: boolean }) : { amount: number; isPaid: boolean } {
+function isCheck(obj: { amount: number; isPaid: boolean }) : { amount: number; isPaid: boolean } {
+    console.log("obj ",obj)
     return {amount : 1000, isPaid : false}
 }
 let data = isCheck({amount : 1000, isPaid : false})
