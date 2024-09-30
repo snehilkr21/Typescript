8e6b04c160c554c56fa3439753c3e011751a9666
snehil <snehil@snehils-MacBook-Air.local>
2024-09-30 11:32:32 +0530

    optional property to a function

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var user1 = {
-    name: "Snehil",
-    isActive: true,
-    // 'email' is not provided, and that's okay because it's optional
-};
-var user2 = {
-    name: "Ravi",
-    email: "ravi@gmail.com",
-    isActive: false, // Both required and optional properties are provided
-};
-console.log(user1);
-console.log(user2);
+function printUserInfo(user) {
+    console.log("Name: ".concat(user.name));
+    // Check if 'email' exists before using it
+    if (user.email) {
+        console.log("Email: ".concat(user.email));
+    }
+    else {
+        console.log('Email not provided');
+    }
+}
+printUserInfo({ name: "Snehil" }); // No email provided
+printUserInfo({ name: "Ravi", email: "ravi@gmail.com" }); // Email provided
--- main.ts
+++ main.ts
@@ -1,23 +1,15 @@
-type User = {
-    name: string;         // Required property
-    email?: string;       // Optional property (may or may not be present)
-    isActive: boolean;    // Required property
-};
+function printUserInfo(user: { name: string; email?: string }) {
+    console.log(`Name: ${user.name}`);
 
-let user1: User = {
-    name: "Snehil",
-    isActive: true,
-    // 'email' is not provided, and that's okay because it's optional
-};
+    // Check if 'email' exists before using it
+    if (user.email) {
+        console.log(`Email: ${user.email}`);
+    } else {
+        console.log('Email not provided');
+    }
+}
 
-let user2: User = {
-    name: "Ravi",
-    email: "ravi@gmail.com",
-    isActive: false,  // Both required and optional properties are provided
-};
-
-console.log(user1);
-console.log(user2);
-
+printUserInfo({ name: "Snehil" });          // No email provided
+printUserInfo({ name: "Ravi", email: "ravi@gmail.com" });  // Email provided
 
 export {}
