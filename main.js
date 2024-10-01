b9fd87cbe5ac7b17232e05dab0c1883ad98f2242
snehil <snehil@snehils-MacBook-Air.local>
2024-10-01 11:32:57 +0530

    earlier it is working but now it is throwing error

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var data1 = [1, 2, 3]; // it suggest that it can store only number
-var data2 = ["1", "2", "3"]; // it reflect that it can only store the string
-var data3 = [1, 2, 3]; //it either store the number or string
-var data4 = [1, 2, "3"]; // it store both number and string
+var newUser = [1, "snehil"];
+newUser.push(false);
--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
- let data1 : number[] = [1,2,3] // it suggest that it can store only number
- let data2 : string[] = ["1","2","3"] // it reflect that it can only store the string
- let data3 : string[] | number[] = [1,2,3] //it either store the number or string
- let data4 : (string | number) [] = [1,2,"3"] // it store both number and string
+type User = [number,string]
+let newUser : User = [1,"snehil"]
+newUser.push(false)
 
-
-export {};
+export {}
