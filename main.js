d048dddb4717f9d8557e028d30bb99d1a522717c
snehil <snehil@snehils-MacBook-Air.local>
2024-10-03 12:37:42 +0530

    generic

--- main.js
+++ main.js
@@ -1,59 +1,22 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
-var Animal = /** @class */ (function () {
-    function Animal(name) {
-        this.name = name; // Initialize the name in the constructor
-    }
-    Animal.prototype.getName = function () { return this.name; }; // Allows subclasses to access the private property
-    Animal.prototype.move = function () {
-        console.log("".concat(this.getName(), " is moving...")); // Use the getter to access the name
-    };
-    return Animal;
-}());
-var Dog = /** @class */ (function (_super) {
-    __extends(Dog, _super);
-    function Dog(name) {
-        return _super.call(this, name) || this; // Call the constructor of the abstract class
-    }
-    Dog.prototype.makeSound = function () { console.log("Woof!"); };
-    Dog.prototype.printName = function () {
-        console.log("Dog's name is: ".concat(this.getName())); // Calling getName() directly
-    };
-    return Dog;
-}(Animal));
-var Cat = /** @class */ (function (_super) {
-    __extends(Cat, _super);
-    function Cat(name) {
-        return _super.call(this, name) || this; // Call the constructor of the abstract class
-    }
-    Cat.prototype.makeSound = function () {
-        console.log("Meow!");
-    };
-    Cat.prototype.printName = function () {
-        console.log("Cat's name is: ".concat(this.getName())); // Calling getName() directly
-    };
-    return Cat;
-}(Animal));
-var dog = new Dog("Buddy");
-var cat = new Cat("Whiskers");
-dog.makeSound(); // Output: Woof!
-dog.move(); // Output: Buddy is moving...
-dog.printName(); // Output: Dog's name is: Buddy
-cat.makeSound(); // Output: Meow!
-cat.move(); // Output: Whiskers is moving...
-cat.printName(); // Output: Cat's name is: Whiskers
+var Score = [];
+var Names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+function identityFour(val) {
+    return val;
+}
+function identityFive(val) {
+    return val;
+}
+identityFive({ brand: "Apple" });
+identityThree("Snehil");
+identityThree(2);
--- main.ts
+++ main.ts
@@ -1,43 +1,31 @@
-abstract class Animal {
-    private name: string; // Private property
-    constructor(name: string) {
-        this.name = name; // Initialize the name in the constructor
-    }
-    protected getName(): string { return this.name; } // Allows subclasses to access the private property
-    abstract makeSound(): void; // Abstract method
-    move(): void {
-        console.log(`${this.getName()} is moving...`); // Use the getter to access the name
-    }
-}
-class Dog extends Animal {
-    constructor(name: string) {
-        super(name); // Call the constructor of the abstract class
-    }
-    makeSound(): void {console.log("Woof!");}
-    printName(): void {
-        console.log(`Dog's name is: ${this.getName()}`); // Calling getName() directly
-    }
-}
-class Cat extends Animal {
-    constructor(name: string) {
-        super(name); // Call the constructor of the abstract class
-    }
-    makeSound(): void {
-        console.log("Meow!");
-    } 
-    printName(): void {
-        console.log(`Cat's name is: ${this.getName()}`); // Calling getName() directly
-    }
-}
-const dog = new Dog("Buddy");
-const cat = new Cat("Whiskers");
+const Score : Array<number> = []
+const Names : Array<string> = []
 
-dog.makeSound(); // Output: Woof!
-dog.move();      // Output: Buddy is moving...
-dog.printName(); // Output: Dog's name is: Buddy
+function identityOne(val :boolean|number) :boolean | number {
+    return val
+}
 
-cat.makeSound(); // Output: Meow!
-cat.move();      // Output: Whiskers is moving...
-cat.printName(); // Output: Cat's name is: Whiskers
+function identityTwo(val : any) : any{
+    return val;
+}
 
+function identityThree<Type>(val : Type) : Type {
+    return val
+}
+
+function identityFour<T>(val : T) : T{
+    return val;
+}
+
+function identityFive<T>(val: T) : T{
+    return val
+}
+interface Bootle {
+    brand : string
+}
+
+identityFive<Bootle>({brand : "Apple"})
+
+identityThree<string>("Snehil")
+identityThree<number>(2)
 export {}
