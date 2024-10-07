c789eb2769295afc7ef1533f41ffcf18d171d9e6
snehil <snehil@snehils-MacBook-Air.local>
2024-10-07 11:48:46 +0530

    extends

--- main.js
+++ main.js
@@ -1,42 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Score = [];
-var Names = [];
-function identityOne(val) {
-    return val;
+function anotherFunction(val1, val2) {
+    return {
+        val1: val1,
+        val2: val2
+    };
 }
-function identityTwo(val) {
-    return val;
-}
-function identityThree(val) {
-    return val;
-}
-function identityFour(val) {
-    return val;
-}
-function identityFive(val) {
-    return val;
-}
-identityFive({ brand: "Apple" });
-identityThree("Snehil");
-identityThree(2);
-//Example 1
-function ProductList(products) {
-    return products[3];
-}
-console.log(ProductList([1, 2, 3, 4, 5]));
-//Example 2
-function ProductList2(products) {
-    return products;
-}
-console.log(ProductList2([1, 2, 3]));
-//Example 3
-var ProductList3 = function (product) {
-    return product[0];
-};
-console.log(ProductList3([1, 2, 3]));
-//Example 4
-var ProductList4 = function (product) {
-    return 0;
-};
-console.log(ProductList4([1, 2, 3]));
+console.log(anotherFunction(3, {
+    username: "snehil",
+    password: "snehil",
+    security: "hi"
+}));
--- main.ts
+++ main.ts
@@ -1,56 +1,19 @@
-const Score : Array<number> = []
-const Names : Array<string> = []
-
-function identityOne(val :boolean|number) :boolean | number {
-    return val
+interface Database {
+    username : string,
+    password : string
 }
 
-function identityTwo(val : any) : any{
-    return val;
+function anotherFunction<T, U extends Database> (val1: T , val2 : U) : object {
+    return {
+        val1,
+        val2
+    }
 }
 
-function identityThree<Type>(val : Type) : Type {
-    return val
-}
+console.log(anotherFunction(3,{ 
+    username : "snehil",
+    password : "snehil",
+    security : "hi"
+ }))
 
-function identityFour<T>(val : T) : T{
-    return val;
-}
-
-function identityFive<T>(val: T) : T{
-    return val
-}
-interface Bootle {
-    brand : string
-}
-
-identityFive<Bootle>({brand : "Apple"})
-
-identityThree<string>("Snehil")
-identityThree<number>(2)
-
-//Example 1
-function ProductList<Type>(products: Type[]): Type {
-    return products[3]; 
-}
-console.log(ProductList([1, 2, 3, 4, 5])); 
-
-//Example 2
-function ProductList2<T>(products : T[]) :T[]{
-    return products
-}
-console.log(ProductList2([1 ,2,3]))
-
-//Example 3
-const ProductList3 = <Type> (product : Type[]) : Type => {
-     return product[0]
-} 
-console.log(ProductList3([1,2,3]))
-
-
-//Example 4
-const ProductList4 = <Type> (product : Type[]) : number => {
-    return 0
-} 
-console.log(ProductList4([1,2,3]))
-export {}
+ export {}
